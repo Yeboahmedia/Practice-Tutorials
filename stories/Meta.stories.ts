dcb9e3a27f645db7ff1bee69d620a10566485191
Yeboahmedia <yeboahmedia@gmail.com>
2024-08-31 15:12:35 +0100

    meta story

---
+++ stories/Meta.stories.ts
@@ -0,0 +1,10 @@
+import { Meta } from './Meta';
+
+export default {
+  title: 'Example/Meta',
+  component: Meta,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
